c1b82fdcd1c1b5f27b83172d45adfac0092761d3
Kittisak Priabying <58898362+kittisakpriabying@users.noreply.github.com>
2021-10-08 15:47:09 +0700

    update

--- project/js/thaiAPI.js
+++ project/js/thaiAPI.js
@@ -1,49 +1,53 @@
-//setup 
-const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
+const thai = new XMLHttpRequest();
+thai.open('GET', 'https://corona.lmao.ninja/v2/countries/thailand');
+thai.send();
+var daily_case, daily_death, daily_recover, total_case, otal_death, total_recover = '';
+thai.onload = () => {
+  console.log(thai);
+  if (thai.status == 200) {
+    var data = JSON.parse(thai.response);
+    daily_case = String(data.todayCases);
+    document.getElementById('dailyCase').innerHTML = '<br> + ' + (daily_case);
+    daily_recover = String(data.todayRecovered);
+    document.getElementById('dailyRecover').innerHTML = '<br> + ' + (daily_recover);
+    daily_death = String(data.todayDeaths);
+    document.getElementById('dailyDeath').innerHTML = '<br> + ' + (daily_death);
+    total_case = String(data.cases);
+    document.getElementById('totalCase').innerHTML = '<br> ' + (total_case);
+    total_death = String(data.deaths);
+    document.getElementById('totalDeath').innerHTML = '<br> ' + (total_death);
+    total_recover = String(data.recovered);
+    document.getElementById('totalRecover').innerHTML = '<br>  ' + (total_recover);
+  }
+}
 
-  };
+function today_function() {
+  document.getElementById('dailyCase').innerHTML = '<br> + ' + (daily_case);
+  document.getElementById('dailyRecover').innerHTML = '<br> + ' + (daily_recover);
+  document.getElementById('dailyDeath').innerHTML = '<br> + ' + (daily_death);
+  document.getElementById('totalCase').innerHTML = '<br> ' + (total_case);
+  document.getElementById('totalDeath').innerHTML = '<br>  ' + (total_death);
+  document.getElementById('totalRecover').innerHTML = '<br>  ' + (total_recover);
+};
 
-  // config
-  const config = {
-    type: 'line',
-    data: data,
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Chart.js Line Chart'
-        }
-      }
-    },
-  };
+function yester_function() {
+  document.getElementById('dailyCase').innerHTML = '<br> + ' + (daily_death);
+  document.getElementById('dailyRecover').innerHTML = '<br> + ' + (daily_death);
+  document.getElementById('dailyDeath').innerHTML = '<br> + ' + (daily_case);
+  document.getElementById('totalCase').innerHTML = '<br> ' + (total_recover);
+  document.getElementById('totalDeath').innerHTML = '<br> ' + (total_death);
+  document.getElementById('totalRecover').innerHTML = '<br> ' + (total_case);
+};
 
-  const myChart = new Chart(
-    document.getElementById("myChart"),
-    config
-  );
+
+var months = new Array();
+            months[1] = "January"; months[2] = "February"; months[3] = "March";
+            months[4] = "April"; months[5] = "May"; months[6] = "June";
+            months[7] = "July"; months[8] = "August"; months[9] = "September";
+            months[10] = "October"; months[11] = "November"; months[12] = "December";
+            var todaysdate = new Date();
+            var date = todaysdate.getDate(); var day = todaysdate.getDay() + 1;
+            var month = todaysdate.getMonth() + 1; var yy = todaysdate.getYear();
+            var year = (yy < 1000) ? yy + 1900 : yy;
+            var year2 = year - (2000 * 1); year2 = (year2 < 10) ? "0" + year2 : year2;
+            document.getElementById("datetime").innerHTML = date + " " + months[month] + ", " + year;
